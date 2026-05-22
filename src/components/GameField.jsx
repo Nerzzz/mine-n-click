50730a2ba76b6717e5029dbb3057edd7ed6b1635
Nerzzz <guilhermeschiavolnsantos@gmail.com>
2026-05-22 15:50:44 -0300

    Pequenas correções

--- src/components/GameField.jsx
+++ src/components/GameField.jsx
@@ -6,13 +6,13 @@ import StatusCard from './StatusCard'
 function GameField({click, level}) {
 
     useEffect(() => {
-        
+
     },[level])
 
     return (
         <main id="game_field" className='flex-1 flex p-[20px] justify-center gap-[20px] md:items-center'>
             <div id="block_field" style={{backgroundImage: `url(images/levels/${level}/foreground.webp)`}} className='flex relative justify-center items-center w-[500px] h-[500px]'>
-                <button id='block' style={{backgroundImage: `url(images/levels/${level}/block.webp)`}} className='click' onClick={click}></button>
+                <div id='block' style={{backgroundImage: `url(images/levels/${level}/block.webp)`}} className='click' onMouseDown={click}></div>
             </div>
             <div id='status_bar' className='flex flex-col gap-[20px] max-w-[100px] h-[500px]'>
                 <StatusCard // Current tool display
--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -7,25 +7,35 @@ function Toast({totalClicks, level}) {
     const timerRef = useRef(null)
 
     useEffect(() => {
+        new Audio('/audio/achievement.mp3').play();
+
+        setVisible(true)
+
+        timerRef.current = setTimeout(() => {
+            setVisible(false)
+        }, 5000)
+    },[])
+
+    useEffect(() => {
         const achievement = ACHIEVEMENTS[level]
 
-        if (totalClicks === achievement.min){
+        if (totalClicks >= achievement.min){
             new Audio('/audio/achievement.mp3').play();
 
             setVisible(true)
 
             timerRef.current = setTimeout(() => {
                 setVisible(false)
-            }, 3000)
+            }, 5000)
             
             return () => clearTimeout(timerRef)
         }
 
-    }, [totalClicks])
+    }, [level])
 
     return (
         <div className={`flex gap-[10px] items-center fixed p-[10px] bottom-[10px] left-[10px] rounded-[8px] min-w-[300px] bg-gray border-3 border-light-foreground transition-[200ms] ${visible ? 'translate-x-0' : '-translate-x-[120%]'}`}>
-            <img className='w-[40px] h-[40px]' src={`images/achievements/${ACHIEVEMENTS[level].icon}`} />
+            <img className='w-[40px] h-[40px] pixelate' src={`images/achievements/${ACHIEVEMENTS[level].icon}`} />
             <div className='text-[11pt]'>
                 <h1 className='!text-[#ffe600]'>New achievement!</h1>
                 <p>{ACHIEVEMENTS[level].msg}</p>
